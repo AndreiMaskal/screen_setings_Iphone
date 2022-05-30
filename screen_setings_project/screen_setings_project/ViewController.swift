b13707a466c38fac216c1da6f3cf81f0be3db067
Andrik <masmay.pl@gmail.com>
2022-05-30 13:12:47 +0200

    initial commit

---
+++ screen_setings_project/screen_setings_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screen_setings_project
+//
+//  Created by Andrei Maskal on 30/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
